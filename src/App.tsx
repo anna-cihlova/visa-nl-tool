35a70d56f81ae1c7ba7024d6f3d80f6cb0a91b81
Cihlička <33567545+anna-cihlova@users.noreply.github.com>
2025-07-15 17:52:12 -0500

    Improved Code in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,34 @@
 import React, { useEffect, useState } from "react";
-import "./App.css";
+import "./App.scss";
 import { Typography } from "@visa/nova-react";
 import { InputCard, OutputCard, VerticalNavigation } from "./components/index";
+
+type Snippet = {
+  title: string;
+  code: string;
+};
+
+type ComponentMeta = {
+  name: string;
+  description: string;
+  keywords: string[];
+  components: string[];
+};
 
 function App() {
   const [inputValue, setInputValue] = useState("");
   const [suggestedComponents, setSuggestedComponents] = useState<string[]>([]);
-  const [codeSnippet, getCodeSnippet] = useState("");
   const [selectedComponents, setSelectedComponents] = useState<string[]>([]);
-  const [savedSnippets, setSavedSnippets] = useState(() => {
+  const [codeSnippet, setCodeSnippet] = useState("");
+  const [savedSnippets, setSavedSnippets] = useState<any>(() => {
     return JSON.parse(localStorage.getItem("savedSnippets") || "[]");
   });
-  const [componentMetadata, setComponentMetadata] = useState<any[]>([]);
+  const [componentMetadata, setComponentMetadata] = useState<ComponentMeta[]>(
+    []
+  );
 
   // Fetch Mock Metadata from Local Mock Server
+  // mock-server.js
   useEffect(() => {
     try {
       const fetchData = async function () {
@@ -30,73 +45,55 @@ function App() {
 
   // Generate Code Snippet
   const generateCode = (components: string[]) => {
+    if (components.length === 0) return "/* Select Components */";
+
     const imports = `import { ${components.join(
       ", "
-    )} } from "@visa/nova-react" \n\n`;
-    const jsx = components
-      .map((comp) => {
-        return `<${comp}/> \n`;
-      })
-      .join(" ");
-    if (components.length === 0) return "/* Select Components */";
-    return `${imports}function MyComponent() {\n  return (\n${jsx});\n}`;
+    )} } from "@visa/nova-react";\n\n`;
+    const jsx = components.map((comp) => `  <${comp} />`).join("\n");
+
+    return `${imports}function MyComponent() {\n  return (\n${jsx}\n  );\n}`;
   };
 
-  // Handle on Submit - Generate Components
+  // Generate List of Components
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
 
-    // Get Suggested Components
-    const cleanInputValue = inputValue.trim().toLowerCase();
-
-    // Find One Matching UI
-    const sortedComponents = componentMetadata
-      .map((comp) => {
-        const score = comp.keywords.reduce(
-          (acc: number, currKeyword: string) => {
-            return acc + (cleanInputValue.includes(currKeyword) ? 1 : 0);
-          },
+    const query = inputValue.trim().toLowerCase();
+    // Sort Suggested Components
+    const sorted = componentMetadata
+      .map((meta) => ({
+        ...meta,
+        score: meta.keywords.reduce(
+          (acc, keyword) => acc + (query.includes(keyword) ? 1 : 0),
           0
-        );
-        return { ...comp, score };
-      })
-      .filter((comp) => comp.score > 0)
+        ),
+      }))
+      .filter((meta) => meta.score > 0)
       .sort((a, b) => b.score - a.score);
-
-    const matchedComponents =
-      sortedComponents.length > 0 ? sortedComponents[0].components : [];
-
-    const componentsToUse =
-      matchedComponents.length > 0
-        ? matchedComponents
-        : ["Container", "Heading", "Text"];
-
-    setSuggestedComponents(componentsToUse);
-    setSelectedComponents(componentsToUse);
-    getCodeSnippet(generateCode(componentsToUse));
+    // Render Component
+    const matched = sorted[0]?.components || ["Container", "Heading", "Text"];
+    setSuggestedComponents(matched);
+    setSelectedComponents(matched);
+    setCodeSnippet(generateCode(matched));
   };
 
+  // Save UI
   const handleSave = () => {
-    // Adding New Snippet
-    const newSnippet = {
+    const newSnippet: Snippet = {
       title: `Saved UI ${savedSnippets.length + 1}`,
       code: codeSnippet,
     };
-
-    // Updating Snippets & Saved Snippets' State
-    const updatedSnippets = [...savedSnippets, newSnippet];
-    localStorage.setItem("savedSnippets", JSON.stringify(updatedSnippets));
-    setSavedSnippets(updatedSnippets);
+    const updated = [...savedSnippets, newSnippet];
+    localStorage.setItem("savedSnippets", JSON.stringify(updated));
+    setSavedSnippets(updated);
   };
 
+  // Delete UI
   const handleDelete = (index: number) => {
-    // Filtering out saved ui with matching index
-    const updatedSnippets = savedSnippets.filter(
-      (_: string, i: number) => i !== index
-    );
-    // Updating Snippets & Saved Snippets' State
-    localStorage.setItem("savedSnippets", JSON.stringify(updatedSnippets));
-    setSavedSnippets(updatedSnippets);
+    const updated = savedSnippets.filter((_: string, i: number) => i !== index);
+    localStorage.setItem("savedSnippets", JSON.stringify(updated));
+    setSavedSnippets(updated);
   };
 
   return (
@@ -120,7 +117,7 @@ function App() {
               suggestedComponents={suggestedComponents}
               selectedComponents={selectedComponents}
               setSelectedComponents={setSelectedComponents}
-              getCodeSnippet={getCodeSnippet}
+              setCodeSnippet={setCodeSnippet}
               generateCode={generateCode}
               codeSnippet={codeSnippet}
               handleSave={handleSave}
